f20c936fd591ff4f69dc635ecfe45ed59f2d928d
Mahaub Alam <mahabubalom72091@gmail.com>
2025-06-16 06:56:32 +0300

    added some code

--- client/src/components/From/AddRoomForm.jsx
+++ client/src/components/From/AddRoomForm.jsx
@@ -1,7 +1,15 @@
 import { categories } from "../Categories/CategoriesData";
 import { DateRange } from "react-date-range";
 import { useState } from "react";
-const AddRoomForm = ({ dates, handelDates, handelerSubmit }) => {
+const AddRoomForm = ({
+  dates,
+  handelDates,
+  handleSubmit,
+  setImagePreview,
+  imagePreview,
+  handleImage,
+  imageText,
+}) => {
   const [state, setState] = useState([
     {
       startDate: new Date(),
@@ -11,7 +19,7 @@ const AddRoomForm = ({ dates, handelDates, handelerSubmit }) => {
   ]);
   return (
     <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
-      <form onSubmit={handelerSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <div className="space-y-6">
             <div className="space-y-1 text-sm">
@@ -74,23 +82,32 @@ const AddRoomForm = ({ dates, handelDates, handelerSubmit }) => {
               />
             </div>
 
-            <div className=" p-4 bg-white w-full  m-auto rounded-lg">
+            <div className=" p-4 bg-white w-full  m-auto rounded-lg flex justify-between items-center">
               <div className="file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg">
                 <div className="flex flex-col w-max mx-auto text-center">
                   <label>
                     <input
                       className="text-sm cursor-pointer w-36 hidden"
                       type="file"
+                      onChange={(e) => handleImage(e.target.files[0])}
                       name="image"
                       id="image"
                       accept="image/*"
                       hidden
                     />
                     <div className="bg-rose-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-rose-500">
-                      Upload Image
+                      {/* {imageText} */}
+                      {imageText.length > 20
+                        ? imageText.split(".")[0].slice(0, 15) +
+                          "...." +
+                          imageText.split(".")[1]
+                        : imageText}
                     </div>
                   </label>
                 </div>
+              </div>
+              <div className="h-16 w-16 object-cover overflow-hidden flex items-center">
+                {imagePreview && <img src={imagePreview} />}
               </div>
             </div>
             <div className="flex justify-between gap-2">
--- client/src/pages/Dashbord/Host/AddRoom.jsx
+++ client/src/pages/Dashbord/Host/AddRoom.jsx
@@ -4,47 +4,72 @@ import useAuth from "../../../hooks/useAuth";
 import { imageUplode } from "../../../api/utils";
 
 const AddRoom = () => {
-    const {user} = useAuth()
+  const { user } = useAuth();
+
+  const [imagePreview, setImagePreview] = useState();
+  const [imageText, setImageText] = useState("Upload Image");
+
   const [dates, setDate] = useState({
     startDate: new Date(),
     endDate: null,
     key: "selection",
   });
-    
-    // date range hanelaer
-    const handelDates = (item) => {
-      console.log(item);
-      setDate(item.selection);
+
+  // date range hanelaer
+  const handelDates = (item) => {
+    console.log(item);
+    setDate(item.selection);
+  };
+
+  // form handler
+  //   Form handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const location = form.location.value;
+    const category = form.category.value;
+    const title = form.title.value;
+    const to = dates.endDate;
+    const from = dates.startDate;
+    const price = form.price.value;
+    const guests = form.total_guest.value;
+    const bathrooms = form.bathrooms.value;
+    const description = form.description.value;
+    const bedrooms = form.bedrooms.value;
+    const image = form.image.files[0];
+
+    const host = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
     };
 
-    // form handler
-    const handelerSubmit = async e => {
-        e.preventDefault()
-
-        const form = e.target
-        const location = form.location.value
-        const category = form.category.value
-        const title = form.title.value
-        const to = ''
-        const from = ''
-        const price = form.price.value
-        const quests = form.total_quest.value
-        const bathrooms = form.bathrooms.value
-        const discription = form.discription.value
-        const bedrooms = form.bedrooms.value
-        const image = form.image.files[0]
-        const host = {
-          name: user?.displayName,
-            image: user?.photoURL,
-          email:user?.email
-        };
-            
-        const imge_url = await imageUplode(image);
-        console.log(imge_url)
-
+    try {
+      const image_url = await imageUplode(image);
+      const roomData = {
+        location,
+        category,
+        title,
+        to,
+        from,
+        price,
+        guests,
+        bathrooms,
+        bedrooms,
+        host,
+        description,
+        image: image_url,
+      };
+      console.table(roomData);
+    } catch (err) {
+      console.log(err);
     }
-
-
+  };
+  //   handle image change
+  const handleImage = (image) => {
+    setImagePreview(URL.createObjectURL(image));
+    setImageText(image.name);
+  };
 
   return (
     <div>
@@ -52,7 +77,11 @@ const AddRoom = () => {
       <AddRoomForm
         dates={dates}
         handelDates={handelDates}
-        handelerSubmit={handelerSubmit}
+        handleSubmit={handleSubmit}
+        setImagePreview={setImagePreview}
+        imagePreview={imagePreview}
+        handleImage={handleImage}
+        imageText={imageText}
       />
     </div>
   );
